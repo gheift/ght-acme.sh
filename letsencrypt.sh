68a51fa27d7ad730c729b872c4aa7c05c5e39009
Attila Bruncsak <bruncsak@users.noreply.github.com>
2018-05-03 09:25:12 +0200

    Bumping version date.

--- letsencrypt.sh
+++ letsencrypt.sh
@@ -105,7 +105,7 @@ IPV_OPTION=
 CHALLENGE_TYPE="http-01"
 
 # the date of the that version
-VERSION_DATE="2018-03-29"
+VERSION_DATE="2018-05-03"
 
 # The meaningful User-Agent to help finding related log entries in the boulder server log
 USER_AGENT="bruncsak/ght-acme.sh $VERSION_DATE"
